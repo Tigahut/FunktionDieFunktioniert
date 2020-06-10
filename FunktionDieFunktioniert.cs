a1f71e8179dc954518561752f21ddd1d7d5baf0b
Tigahut <63925758+Tigahut@users.noreply.github.com>
2020-06-10 19:06:06 +0200

    Add files via upload

---
+++ FunktionDieFunktioniert.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Dynamic;
+//TTTimo
+namespace FSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        private static string FSC(string searched, string sword)
+        {
+            int LwLenght = sword.Length;
+            int LeCounter = 0;
+            int Lwcount = 0;
+            string returnValue = searched + " kommt an den Stellen: ";
+            while(LeCounter != LwLenght)
+            {
+                if(searched == Convert.ToString(sword[LeCounter]))
+                {
+                    returnValue += Convert.ToString(LeCounter) + " ,";
+                    Lwcount++;
+                }
+                LeCounter++;
+            }
+            returnValue = returnValue.TrimEnd(',');
+            returnValue += "vor. Das ist " + Lwcount + " mal!";
+            return returnValue;
+        }
+    }
+}
